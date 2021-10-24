0bf45589c72639214165e42f63c4f66314ec7416
rhx1138 <ryanjhendrix@gmail.com>
2021-10-24 16:42:25 -0400

    updated nav color and hero background

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ const navUL = document.getElementById('nav-ul');
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 })
+
+function svgToBase64Image(svgElement) {
+    var div = document.createElement('div');
+    div.appendChild(svgElement.cloneNode(true));
+    var b64 = window.btoa(div.innerHTML);
+    return 'data:image/svg+xml;base64,' + b64;
+  }
+  var svgs = document.getElementsByTagName('svg');
+  var urls = [];
+  for (var i = 0; i < svgs.length; i++)
+    urls.push('url("' + svgToBase64Image(svgs[i]) + '")');
+  var url = urls.join(',');
+  var msg = 'Base64 CSS image (for cross-browser support):' +
+            '<br><br>background: ' + url + ';';
+  document.getElementById('code').innerHTML = msg;
+  document.body.style.background = url;
