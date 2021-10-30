b383d147ab715800cd436d3f1e5f2c6ad266e902
rhx1138 <ryanjhendrix@gmail.com>
2021-10-30 11:28:54 -0400

    added scroll effect to features and privacy links

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 let downloadLink = document.querySelector("#downloadLink");
 let download = document.querySelector("#download");
 
+let featureLink = document.querySelector("#featuresLink");
+let feature = document.querySelector("#features");
+
+let privacyLink = document.querySelector("#privacyLink");
+let privacy = document.querySelector("#privacy");
+
+
 downloadLink.addEventListener("click", navigateDownload, false);
 
 function navigateDownload(e) {
-  download.scrollIntoView({behavior: "smooth"});
+  download.scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+featuresLink.addEventListener("click", navigateFeatures, false);
+
+function navigateFeatures(e) {
+  features.scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+privacyLink.addEventListener("click", navigatePrivacy, false);
+
+function navigatePrivacy(e) {
+  privacy.scrollIntoView({
+    behavior: "smooth"
+  });
 }
