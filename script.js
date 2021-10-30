82c28975a389ab9a77a4b488f13592af6bbfb1a7
rhx1138 <ryanjhendrix@gmail.com>
2021-10-30 11:08:17 -0400

    added js animated scroll to download link

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// hamburger controller
+let downloadLink = document.querySelector("#downloadLink");
+let download = document.querySelector("#download");
 
-// const hamburger = document.getElementById('hamburger');
-// const navUL = document.getElementById('nav-ul');
+downloadLink.addEventListener("click", navigateDownload, false);
 
-<<<<<<< HEAD
-hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show');
-})
-
-function svgToBase64Image(svgElement) {
-    var div = document.createElement('div');
-    div.appendChild(svgElement.cloneNode(true));
-    var b64 = window.btoa(div.innerHTML);
-    return 'data:image/svg+xml;base64,' + b64;
-  }
-  var svgs = document.getElementsByTagName('svg');
-  var urls = [];
-  for (var i = 0; i < svgs.length; i++)
-    urls.push('url("' + svgToBase64Image(svgs[i]) + '")');
-  var url = urls.join(',');
-  var msg = 'Base64 CSS image (for cross-browser support):' +
-            '<br><br>background: ' + url + ';';
-  document.getElementById('code').innerHTML = msg;
-  document.body.style.background = url;
-=======
-// hamburger.addEventListener('click', () => {
-//     navUL.classList.toggle('show');
-// })
->>>>>>> feature-navbar-two
+function navigateDownload(e) {
+  download.scrollIntoView({behavior: "smooth"});
+}
